86a574b4217892df0c60b36072d74a62d80bf4de
aaron <jeroo@qq.com>
2016-11-18 10:35:33 +0800

    day03表单验证与依赖注入

---
+++ h5c3/angular/js/controller.js
@@ -0,0 +1,14 @@
+angular.module('mainCtrl',[])
+.controller('homeController',[
+	'$scope',
+	function($scope){
+		$scope.name = '丝绸之路'
+	}]
+)
+
+.controller('listController', [
+	'$scope', 
+	function($scope){
+		
+	}
+])
